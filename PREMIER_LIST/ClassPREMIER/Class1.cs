8bd7c175a888c156ecb6f8411d15e1dada339779
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-10 15:55:08 +0200

    PREMIER liste

---
+++ PREMIER_LIST/ClassPREMIER/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPREMIER
+{
+    public class Class1
+    {
+        public static bool Nombrepremier(int _Nbutilisateur)
+
+        {
+            double reste;
+            int Diviseur = 2;
+            bool Premier = true;
+            double Limite = Math.Sqrt(_Nbutilisateur);
+
+            do
+            {
+                reste = _Nbutilisateur % Diviseur;
+
+                Diviseur++;
+
+                if (reste == 0 && _Nbutilisateur != 2)
+                {
+                    Premier = false;
+                    break;                   
+                }
+                
+
+            } while (Diviseur <= Limite);
+
+            return Premier;
+        }
+        public static List<int> GivePremier(int Nbr)
+        {
+            List<int> tab = new List<int>();
+
+            for (int i = 0; i < Nbr; i++)
+            {
+                
+                if (Nombrepremier(i) == true && i != 1)
+                {
+                    tab.Add(i);
+                }
+            }
+            return tab;
+        }
+    }
+}
--- PREMIER_LIST/PREMIER_LIST/Program.cs
+++ PREMIER_LIST/PREMIER_LIST/Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using ClassPREMIER;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PREMIER_LIST
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int nb;
+            
+
+            Console.WriteLine("Entrez le nombre de Premiers désirés:");
+            nb = int.Parse(Console.ReadLine());
+
+            List<int> r = Class1.GivePremier(nb);
+
+            for (int i = 0; i < r.Count; i++)
+            {
+                Console.WriteLine(r[i]);
+            }
+            
+
+            Console.ReadKey();
+
         }
+        
+        
+        
     }
 }
