4fb00b189106eeea9aea7946fa4fd3a4417cca14
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-11 12:50:43 +0200

    Jeunes

---
+++ JEUNES/JEUNES/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEUNES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] jeunes = new int[20];
+
+            for (int i = 0; i < jeunes.Length; i++)
+            {
+                Console.WriteLine("Entrez l'âge de la personne n°" + (i + 1));
+                jeunes[i] = int.Parse(Console.ReadLine());
+            }
+            int moinsdevingt = 0;
+
+            foreach(int age in jeunes)
+            {
+                if (age < 20)
+                {
+                    moinsdevingt++;
+                }
+            }
+
+            Console.WriteLine("Les personnes ayant moins de vingt ans sont au nombre de : "+ moinsdevingt);
+
+            Console.ReadKey();
+        }
+    }
+}
