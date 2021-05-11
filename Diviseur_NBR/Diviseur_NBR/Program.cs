110ae90d2589e9e8ad2ce7e975d5082911497a32
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-11 10:02:45 +0200

    Liste diviseurs d'un nombre

---
+++ Diviseur_NBR/Diviseur_NBR/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diviseur_NBR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NBR;
+            Console.WriteLine("Entrez un nombre entier:");
+            NBR = int.Parse(Console.ReadLine());
+
+            List<int> r= Diviseur(NBR);
+
+            Console.WriteLine("Les diviseurs de " + NBR + " sont:");
+
+            for (int i = 0; i < r.Count; i++)
+            {
+                Console.WriteLine(r[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+        public static List<int> Diviseur(int _nbr)
+        {
+            int diviseur = 2;
+            List<int> Listing = new List<int>();
+
+            do
+            {
+                if (_nbr % diviseur == 0)
+                {
+                    Listing.Add(diviseur);
+                }
+                diviseur++;
+
+            } while (diviseur < _nbr);
+
+            return Listing;
+        }
+    }
+}
