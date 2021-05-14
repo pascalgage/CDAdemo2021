28f9e35924adf120c64242ddfd54a4d5da0f5886
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-14 14:40:41 +0200

    nouveau projet PREMIER NBR

---
+++ PREMIER_LIST/PREMIERNBR/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassPREMIER;
+
+namespace PREMIERNBR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint nombre;
+            
+            bool test;
+
+            Console.WriteLine("Entrez un nombre entier positif pour vérifier s'il est PREMIER:");
+            nombre = uint.Parse(Console.ReadLine());
+
+            test=Class1.EstUnNombrepremier(nombre);
+
+            if (test == true) { Console.WriteLine("le nombre " + nombre + " est PREMIER"); }
+            else
+            {
+                Console.WriteLine("le nombre " + nombre + " n'est pas PREMIER");
+            }
+
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
---
+++ PREMIER_LIST/PREMIER_UN_NOMBRE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassPREMIER;
+
+namespace PREMIER_UN_NOMBRE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
