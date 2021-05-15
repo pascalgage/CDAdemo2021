560ec0fe4ddc2dd492f34f2a82a9007a5d322479
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-15 15:36:29 +0200

    nouveau projet Search

---
+++ SEARCH_inTAB/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEARCH_inTAB
+{
+    class Class1
+    {
+        public static string Search_inTab(List<string> tab, string mot)
+        {
+            string resultat = " ";
+            int compteur=0;
+            List <int> position= new List<int>();
+
+
+            //Début de la rechcerche dans le tableau tab en paramètre du mot en paramètre
+            for (int i = 0; i < tab.Count; i++)
+            {
+                if (tab[i] == mot)
+                {
+                    compteur++;
+                    position.Add(i + 1);
+                }
+            }
+            //fin de boucle de recherche dans le tableau tab passé en paramètre
+            if (tab.Count == 0)
+            {
+                resultat = "Le tableau est vide";
+            }
+            if (compteur == 0)
+            {
+                resultat = "L'élément recherché" + mot + " n'existe pas dans le tableau";
+            }
+            else if (compteur == 1)
+            {
+                resultat= "Lélément "+mot+" est présent une seule fois dans le tableau à la position "+position[0];
+            }
+            else
+            {
+
+            }
+
+            return resultat;
+        }
+    }
+}
---
+++ SEARCH_inTAB/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEARCH_inTAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> prenoms = new List<string>();
+            string prenom = "";
+            int nbreleve = 0;
+            int compteur = 0;
+
+
+            Console.WriteLine("Entrez le nombre d'élève de votre liste:");
+            nbreleve = int.Parse(Console.ReadLine());
+
+
+            do
+            {
+                Console.WriteLine("Veuillez entrer vos prénoms:");
+                while ((prenom = Console.ReadLine()) != null)
+                {
+                    prenoms.Add(prenom);
+                    compteur++;
+                }
+            } while (compteur != nbreleve);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
