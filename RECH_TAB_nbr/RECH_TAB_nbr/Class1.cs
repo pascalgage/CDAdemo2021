ebfc4dcbb418bc236df47cc24c8926557c405690
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-17 14:31:32 +0200

    Nouveau

---
+++ RECH_TAB_nbr/RECH_TAB_nbr/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RECH_TAB_nbr
+{
+    class Class1
+    {
+        public static void Tri_Bull(int[] _Tab)
+        {
+            int temp = 0;
+            bool tri = false;
+
+            while (tri == false)
+            {
+                tri = true;
+                for (int i = 0; i < _Tab.Length - 1; i++)
+                {
+                    if (_Tab[i] > _Tab[i + 1])
+                    {
+                        tri = false;
+                        temp = _Tab[i];
+                        _Tab[i] = _Tab[i + 1];
+                        _Tab[i + 1] = temp;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ RECH_TAB_nbr/RECH_TAB_nbr/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RECH_TAB_nbr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            int element = 1;
+
+            int nombreAreCH;
+
+            Console.WriteLine("Entrez la taille de votre tableau, nombre de case= :");
+            index = int.Parse(Console.ReadLine());
+
+            int[] tableau = new int[index];
+
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                Console.WriteLine("Entrez le nombre " + ((element)++) + "/" + index);
+                tableau[i] = int.Parse(Console.ReadLine());
+            }
+
+            Class1.Tri_Bull(tableau);
+
+            foreach (var item in tableau)
+            {
+                Console.Write(" " + item + " ");
+            }
+            Console.WriteLine("Entrez le nombre à rechercher:");
+            nombreAreCH = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] == nombreAreCH)
+                {
+                    Console.WriteLine(+nombreAreCH + " se trouve à la position "+(i+1));
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
