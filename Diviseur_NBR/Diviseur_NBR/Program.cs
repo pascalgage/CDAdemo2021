4b0fe28608d2c236ca1aa4ee7b8b77f8f11f3757
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-17 09:41:12 +0200

    if else ...

--- Diviseur_NBR/Diviseur_NBR/Program.cs
+++ Diviseur_NBR/Diviseur_NBR/Program.cs
@@ -12,25 +12,28 @@ namespace Diviseur_NBR
         static void Main(string[] args)
         {
             uint NBR;
-            Console.WriteLine("Entrez un nombre entier:");
+            Console.WriteLine("Entrez un nombre entier positif:");
             NBR = uint.Parse(Console.ReadLine());
 
+            //Création le la liste des diviseurs du Nombre NBR....
 
             List<uint> r = Diviseur1.Diviseur(NBR);
 
-            Console.WriteLine("Les diviseurs de " + NBR + " sont:");
-
-            for (int i = 0; i < r.Count; i++)
-            {
-                Console.WriteLine(r[i]);
-            }
+            //Traitement du nombre NBR...affichage premier oui/non ou les diviseurs
 
             if (Diviseur1.EstUnNombrepremier(NBR) == true)
             {
                 Console.WriteLine(+NBR + " est premier donc les diviseurs sont 1 et lui-même");
             }
+            else
+            {
+                Console.WriteLine("Les diviseurs de " + NBR + " sont:");
 
-
+                for (int i = 0; i < r.Count; i++)
+                {
+                    Console.WriteLine(r[i]);
+                }
+            }
 
             Console.ReadKey();
 
