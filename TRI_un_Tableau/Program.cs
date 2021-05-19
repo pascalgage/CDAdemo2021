7611c76fd31e31c085e64cf1d1189ed768b51434
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-19 14:54:04 +0200

    Nouvel exercice tab et list à trier !!!

---
+++ TRI_un_Tableau/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRI_un_Tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclaration de la variable taille du tableau
+            int tailleTableau=0;
+            //On demande à l'utilisateur d'entrer la taille du tableau désiré
+            Console.WriteLine("Entrez la taille de votre tableau:");
+            tailleTableau = int.Parse(Console.ReadLine());
+            //déclaration puis instanciation du tableau désiré
+            int[] tableau = new int[tailleTableau];
+            //Remplissage du tableau...
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                Console.WriteLine("Entrez le nombre "+(i + 1)+"/"+tailleTableau+ " de votre tableau: ");
+                tableau[i] = int.Parse(Console.ReadLine());
+            }
+            //Affichage du tableau non trié
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                Console.Write(" "+tableau[i]+" ");
+            }
+            //Traîtement par appel de la fonction tri à bulle
+            Tri.Tri_à_Bulle(tableau);
+            //Affichage du résultat...
+
+            Console.WriteLine(" ");
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                Console.Write(" " + tableau[i] + " ");
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ TRI_un_Tableau/Tri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRI_un_Tableau
+{
+    public class Tri
+    {
+        public static void Tri_à_Bulle(int[] _tableau)
+        {
+            int temp = 0;
+            bool trie = false;
+
+            while (trie==false)
+            {
+                trie = true;
+                for (int i = 0; i < (_tableau.Length)-1; i++)
+                {
+                    if (_tableau[i] > _tableau[i + 1])
+                    {
+                        trie = false;
+                        temp = _tableau[i];
+                        _tableau[i]= _tableau[i + 1];
+                        _tableau[i + 1] = temp;
+
+                    }
+                }
+            }
+        }
+    }
+}
