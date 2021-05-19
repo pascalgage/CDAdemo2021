d1270e8f1b3a5a40c74453a283ac72b8a4861abe
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-19 10:32:15 +0200

    dénombrer OK

--- Denombrer_TAB_ALPHABET/Denombrer_TAB_ALPHABET/Program.cs
+++ Denombrer_TAB_ALPHABET/Denombrer_TAB_ALPHABET/Program.cs
@@ -10,14 +10,52 @@ namespace Denombrer_TAB_ALPHABET
     {
         static void Main(string[] args)
         {
-            string[] Alphabet = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-                                                 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            int[] TabCount = new int[26];
+            string textAexaminer = "";
 
-            string textAexaminer;
 
+            //création du tableau Alphabétique de comparaison...
+            char[] Alphabet = new char[26];
+            char A = 'a';
+
+            for (int i = 0; i < Alphabet.Length; i++)
+            {
+                Alphabet[i] = A;
+                A++;
+            }
+            //création du compteur d'occurences...
+            int[] tableauComptage = new int[26];
+
+            //création et mise en mémoire d'un texte utilisateur en minuscule
             Console.WriteLine("Entrer votre texte:");
-            textAexaminer = Console.ReadLine();
+            textAexaminer = Console.ReadLine().ToLower();
+
+            //on convertit le texte en tableau de caractères en vue de la comparaison avec tableau Alphabet
+            char[] textAexaminerX = textAexaminer.ToCharArray();
+
+            //on effectue la comparaison du tableau de caractère du texte utilisateur avec notre Alphabet
+            for (int i = 0; i < Alphabet.Length; i++)
+            {
+                foreach (char ALPHA in textAexaminerX)
+                {
+
+                    if (Alphabet[i] == ALPHA)
+                    {
+                        tableauComptage[i]++;
+                    }
+
+                }
+            }
+            //Affichage du résultat final...
+            for (int i = 0; i < tableauComptage.Length; i++)
+            {
+                if (tableauComptage[i] != 0)
+                {
+                    Console.WriteLine(Alphabet[i] + " : " + tableauComptage[i]);
+                }
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
