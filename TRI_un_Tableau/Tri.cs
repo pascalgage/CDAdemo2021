cd34b43d662e45edbdae7378f5caef6eea54fecd
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-19 15:50:23 +0200

    Création Tri dans une liste

--- TRI_un_Tableau/Tri.cs
+++ TRI_un_Tableau/Tri.cs
@@ -29,5 +29,25 @@ namespace TRI_un_Tableau
                 }
             }
         }
-    }
+        public static void Tri_à_Bulle_LIST(List<int> _tableau)
+        {
+            int temp = 0;
+            bool trie = false;
+
+            while (trie == false)
+            {
+                trie = true;
+                for (int i = 0; i < (_tableau.Count) - 1; i++)
+                {
+                    if (_tableau[i] > _tableau[i + 1])
+                    {
+                        trie = false;
+                        temp = _tableau[i];
+                        _tableau[i] = _tableau[i + 1];
+                        _tableau[i + 1] = temp;
+
+                    }
+                }
+            }
+        }   }
 }
---
+++ TRI_une_List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRI_un_Tableau;
+
+namespace TRI_une_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclaration et instanciation des variables
+            List<int> Listing = new List<int>();
+            bool finish = false;
+            int nombre;
+            string decisionSaisie;
+
+            while (finish==false)
+            {
+                
+                Console.WriteLine("Entrez vos nombres dans la liste:");
+                nombre = int.Parse(Console.ReadLine());
+
+                Listing.Add(nombre);
+
+                Console.WriteLine("Saisie OK ? o/n");
+                decisionSaisie = Console.ReadLine();
+
+                if (decisionSaisie == "o")
+                {
+                    finish = true;
+                    Console.WriteLine("Nous procédons au traîtement de vos saisies");
+                }
+                else
+                {
+                    Console.WriteLine("Entrez vos nombres dans la liste:");
+                    nombre = int.Parse(Console.ReadLine());
+                    Listing.Add(nombre);
+                }
+
+            }
+            Tri.Tri_à_Bulle_LIST(Listing);
+
+            Console.WriteLine("Votre liste de nombres triés en ordre croissant est :");
+            for (int i = 0; i < Listing.Count; i++)
+            {
+                Console.Write(" "+Listing[i]+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
