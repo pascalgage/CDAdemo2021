117db8525e5ec0e6b6bff89ff1594e0917c66973
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-20 10:36:49 +0200

    Réussi !

---
+++ Le_Palindrome/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Le_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase;
+            int indiceLettre = 0;
+            bool palindrome = true;
+
+
+            Console.WriteLine("Entrez une phrase ou un mot pour voir s'il s'agit d'un palindrome");
+            phrase = Console.ReadLine();
+
+         
+
+            while (palindrome==true && indiceLettre<(phrase.Length/2))
+            {
+
+                indiceLettre++;
+
+                if ((phrase[indiceLettre] == phrase[phrase.Length - 1 - indiceLettre]))
+                {
+                    palindrome = true;
+                   
+                }
+                else
+                {
+                    palindrome = false;
+                    
+                }
+                
+            }
+
+            if (palindrome == true)
+            {
+                Console.WriteLine("Palindrome OK");
+            }
+            else
+            {
+                Console.WriteLine("NO Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
