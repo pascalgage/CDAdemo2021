93c10de44388f65ccc25d0a3ca294edbea078668
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-23 19:18:55 +0200

    Fonction pgcd

---
+++ PGCD_méthode_Last/PGCD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGCD_méthode_Last
+{
+    public class PGCD
+    {
+        public static int Get_Pgcd(int _a, int _b)
+        {
+            int div = 1;
+            List<int> Tab = new List<int>();
+
+            while(div<_a && div < _b)
+            {
+                div++;
+
+                if(_a%div==0 && _b % div == 0)
+                {
+                    Tab.Add(div);
+                }
+            }
+            return Tab.Last();
+        }
+    }
+}
---
+++ PGCD_méthode_Last/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGCD_méthode_Last
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int div;
+
+            Console.WriteLine("Veuillez entrer un nombre entier positif A:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un nombre entier positif B:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write(PGCD.Get_Pgcd(a, b));
+
+            Console.ReadKey();
+
+        }
+    }
+}
