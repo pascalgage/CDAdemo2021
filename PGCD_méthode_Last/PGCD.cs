696da44f4c0b3779c0b8b202375270601bf5dc4f
sthiry2 <sthiry@arfp.asso.fr>
2021-05-25 10:02:29 +0200

    Ajout commentaire sur boucle while et correction nomencalture

--- PGCD_méthode_Last/PGCD.cs
+++ PGCD_méthode_Last/PGCD.cs
@@ -11,18 +11,19 @@ namespace PGCD_méthode_Last
         public static int Get_Pgcd(int _a, int _b, int _c)
         {
             int div=0;
-            List<int> Tab = new List<int>();
+            List<int> tab = new List<int>();
 
+            //qd on sait combien de boucle on fait, on fait un for
             while(div<_a && div < _b && div<_c)
             {
                 div++;
 
                 if(_a % div==0 && _b % div == 0 && _c % div==0)
                 {
-                    Tab.Add(div);
+                    tab.Add(div);
                 }
             }
-            return Tab.Last();
+            return tab.Last();
         }
     }
 }
