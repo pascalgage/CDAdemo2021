3fa205f5967aff50fd75f371bdc3754eea73658f
Pascal GAGE <PGage@arfp.asso.fr>
2021-05-25 09:53:58 +0200

    ajout mardi

---
+++ Dichotomie/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dichotomie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prenom="";
+            string prenomRecherché;
+
+            List<string> prenoms = new List<string>();
+
+            TabCreation.CreeTab(out prenoms);
+
+            for (int i = 0; i < prenoms.Count; i++)
+            {
+                Console.WriteLine(prenoms[i]);
+            }
+
+            Console.WriteLine("Entrez le prénom que vous recherchez pour obtenir son index:");
+            prenomRecherché = Console.ReadLine();
+
+            for (int i = 0; i < prenoms.Count; i++)
+            {
+                if (prenomRecherché == prenoms[i])
+                {
+                    Console.WriteLine(i+1);
+                }
+                else
+                {
+                    Console.WriteLine("Le prénom recherché n'existe pas dans le tableau !");
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dichotomie/TabCreation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dichotomie
+{
+    public class TabCreation
+    {
+        public static void CreeTab(out List<string> Tableau_prenoms)
+        {
+            string verifie="";
+            Tableau_prenoms = new List<string>();
+
+            Console.WriteLine("Entrez un nom pour l'ajouter à la liste:");
+            Tableau_prenoms.Add(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Entrez un nom pour l'ajouter à la liste:");
+                verifie = Console.ReadLine();
+                if (verifie != "")
+                {
+                    Tableau_prenoms.Add(verifie);
+                }
+            } while (verifie != "");
+
+            Tableau_prenoms.Sort();
+        }
+    }
+}
