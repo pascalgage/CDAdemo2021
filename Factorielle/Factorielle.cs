9c84b695ffc011f131cd01f0cd8c998513a2b76c
Pascal GAGE <PGage@arfp.asso.fr>
2021-05-26 17:02:32 +0200

    correction sans recursivité

---
+++ Factorielle/Factorielle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorielle
+{
+    class Factorielle
+    {
+        public static double Factor(double _nbr)
+        {
+            double resultat = _nbr;
+            double _exp = _nbr;
+
+            while (_exp != 1)
+            {
+                _exp--;
+                resultat = resultat * _exp;
+
+            }
+            return resultat;
+        }
+    }
+}
---
+++ Factorielle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorielle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f=0;
+
+            Console.WriteLine("Entrez un nombre F:");
+            f=double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorielle.Factor(f));
+
+            Console.ReadKey();
+        }
+    }
+}
