2e3b3e8f08e2346653e9eb9880370fdb26aebc9f
Pascal GAGE <PGage@arfp.asso.fr>
2021-05-27 11:52:46 +0200

    Mise en place Hanoi

---
+++ Hanoi_OK/Hanoi_Try.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi_OK
+{
+    public class Hanoi_Try
+    {
+        public static void DeplacerPalet(ref string tabOrigine, ref string tabDestination)
+        {
+            char palet = tabOrigine[0];
+            tabOrigine = tabOrigine.Remove(0, 1);
+            tabDestination = tabDestination.Insert(0, palet.ToString());
+
+        }
+
+        public static void Transfert(int nombreDePaletADeplacer, ref string origine, ref string destination, ref string intermediaire)
+        {
+
+        }
+    }
+}
---
+++ Hanoi_OK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi_OK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tA = "12345";
+            string tB = "";
+            string tC = "";
+            Hanoi_Try.Transfert(tA.Length, ref tA, ref tC, ref tB);
+            Console.WriteLine("" + tA + "");
+            Console.WriteLine("" + tB + "");
+            Console.WriteLine("" + tC + "");
+
+            Console.ReadKey();
+        }
+    }
+}
