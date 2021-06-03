4a8a8663091d58ae1db1dae4b43dc1ed98652aef
Gage <72741160+pascalgage@users.noreply.github.com>
2021-06-03 10:49:31 +0200

    entraînement télévision

---
+++ ClassTelevision/PosteDeTelevision.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTelevision
+{
+    public class PosteDeTelevision
+    {
+        //attributs ou champs en C#
+        private bool allume;
+        private int diagonal;
+        //Contructeurs...
+
+        //constructeur par défaut
+        public PosteDeTelevision()
+        {
+            allume = false;
+            diagonal = 17;
+        }
+        //constructeur classique
+        public PosteDeTelevision(bool _allume, int _diagonal)
+        {
+            this.allume = _allume;
+            this.diagonal = _diagonal;
+        }
+        //méthodes en C#
+
+        public void Allumer()
+        {
+            allume = true;
+        }
+        public void Eteindre()
+        {
+            allume = false;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+" allume= "+allume+" diagonal= "+diagonal;
+        }
+    }
+}
---
+++ Television/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassTelevision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Television
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosteDeTelevision lateledusalon;//declaration
+            lateledusalon = new PosteDeTelevision();//initialisation par instanciation
+
+            Console.WriteLine(lateledusalon.ToString());
+
+            lateledusalon.Allumer();
+            lateledusalon.Eteindre();
+        }
+    }
+}
