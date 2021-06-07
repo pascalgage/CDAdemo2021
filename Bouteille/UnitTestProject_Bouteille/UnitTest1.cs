ebabe7e5127994e90d9f222233638e520b2830a3
Gage <72741160+pascalgage@users.noreply.github.com>
2021-06-07 13:24:04 +0200

    test unitaires

--- Bouteille/UnitTestProject_Bouteille/UnitTest1.cs
+++ Bouteille/UnitTestProject_Bouteille/UnitTest1.cs
@@ -21,5 +21,34 @@ namespace UnitTestProject_Bouteille
             NS_Bouteille.Bouteille maBouteille = new NS_Bouteille.Bouteille(0, 0, true);
             Assert.IsTrue(maBouteille.Fermer());
         }
+        [TestMethod]
+        public void Remplir_RemplirQuantité_RetourneTrue()
+        {
+            decimal _aRajouterEnPourcentage = 30;
+            NS_Bouteille.Bouteille maBouteille = new NS_Bouteille.Bouteille(0, 0, true);
+            Assert.IsTrue(maBouteille.Remplir(_aRajouterEnPourcentage));
+        }
+        [TestMethod]
+        public void Remplir_RemplirQuantité_RetourneFalse()
+        {
+            decimal _aRajouterEnPourcentage = 110;
+            NS_Bouteille.Bouteille maBouteille = new NS_Bouteille.Bouteille(0, 0, true);
+            Assert.IsFalse(maBouteille.Remplir(_aRajouterEnPourcentage));
+        }
+        [TestMethod]
+        public void Vider_quantitéValide_retourneTrue()
+        {
+            //crée une bouteille valide
+            Bouteille maBouteille = new Bouteille(2, 50, true);
+            //Définir une quantité valide à vider
+            decimal quantiteAvider = 45;
+            //Vider une quantité valide
+            bool resultat = maBouteille.Vider(quantiteAvider);
+            //verifié que resultat est égal à true
+            Assert.AreEqual(true, resultat);
+            //Assert.IsTrue(resultat)
+        }
+       
+
     }
 }
