e73c7c7f05809d7e07bf4a9c810dcdf5a31f6089
Gage <72741160+pascalgage@users.noreply.github.com>
2021-06-30 13:24:20 +0200

    en cours

--- Le_421/Program.cs
+++ Le_421/Program.cs
@@ -11,13 +11,7 @@ namespace Le_421
     {
         static void Main(string[] args)
         {
-            De monDe = new De();
-            Console.WriteLine(monDe.ToString());
-            monDe.SeJeter();
-            Console.WriteLine(monDe.ToString());
-            monDe.SeJeter();
-            Console.WriteLine(monDe.ToString());
-
+            
             Manche départ = new Manche();
             départ.LancerLes3Des();
             Console.WriteLine(départ.ToString());
