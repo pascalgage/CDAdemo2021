5ee2be70f5db10d2b6548127f590edeba47020b7
Pascal GAGE <PGage@arfp.asso.fr>
2022-01-10 12:57:17 +0100

    ok
    ok

---
+++ CodeFizz/Calcul.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFizz
+{
+    public class Calcul
+    {
+        public static void Fizzbuzz(List<int> maListe)
+        {
+
+
+
+
+            for(int i = 0; i < maListe.Count; i++)
+            {
+                if(maListe[i]%5==0 && maListe[i] % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }else if (maListe[i] % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if (maListe[i] % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(maListe[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ CodeFizz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeFizz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nombres = new List<int>();
+            int nombre=0;
+            bool finish = true;
+            string decision = "n";
+
+            while (finish==true && decision=="n")
+            {
+                Console.WriteLine("Entrez vos nombres :");
+                nombre = int.Parse(Console.ReadLine());
+                nombres.Add(nombre);
+
+                Console.WriteLine("Avez-vous fini votre saisie ?");
+                decision = Console.ReadLine();
+
+                if (decision != "n")
+                {
+                    finish = true;
+                    Calcul.Fizzbuzz(nombres);
+;
+                }
+            }
+            Console.ReadKey();
+            
+            
+            
+        }
+    }
+}
