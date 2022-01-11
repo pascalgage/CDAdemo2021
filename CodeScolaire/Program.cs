798a8698fd040fd3c6f39d2dcdfce923fba2f4b9
Pascal GAGE <PGage@arfp.asso.fr>
2022-01-11 13:02:56 +0100

    Update Program.cs
    ok

--- CodeScolaire/Program.cs
+++ CodeScolaire/Program.cs
@@ -10,21 +10,41 @@ namespace CodeScolaire
     {
         static void Main(string[] args)
         {
-            int nombre = 0;
+            /*int nombre = 0;
             Console.WriteLine("Veuillez saisir un nombre pour obtenir les 10 suivants");
             nombre = int.Parse(Console.ReadLine());
 
             List<int> cible = new List<int>();
 
-            for(int i=nombre; i<=(nombre+10); i++)
+            for(int i=nombre+1; i<=(nombre+10); i++)
             {
                 cible.Add(i);
             }
-
+            Console.Clear();
             for(int i=0; i<cible.Count; i++)
             {
                 Console.WriteLine(cible[i]);
+            }*/
+
+            /*int table = 0;
+            Console.WriteLine("Veuillez saisir le chiffre dont vous souhaitez obtenir la table de multiplication :");
+            table = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(table * i);
+            }*/
+
+            int nbr = 0;
+            int result = 0;
+            Console.WriteLine("Donnez votre nombre de départ: ");
+            nbr = int.Parse(Console.ReadLine());
+
+            for(int i=1; i <= nbr; i++)
+            {
+                result +=i;
             }
+            Console.WriteLine(result);
 
             Console.ReadKey();
         }
